c7e1fcf207563d69814489c453bb575e1bb0a969
chasevanblair <crvanblair@dmacc.edu>
2020-06-17 19:19:25 -0500

    test_score_input_name

--- more_functions/validate_input_functions.py
+++ more_functions/validate_input_functions.py
@@ -1,5 +1,9 @@
-def score_input(test_name, test_score=0, invalid_message="Invalid test score, try again!"):
-   #while 100 < test_score < 0:
-    #    print(invalid_message)
-   #return {test_name: test_score}
-    return pass
+def score_input(test_name: str, test_score=0, invalid_message="Invalid test score, try again!"):
+    while 100 < test_score < 0:
+        print(invalid_message)
+    # return {test_name: test_score}
+
+    return test_name, test_score
+
+
+#score_input()
--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -4,17 +4,29 @@ from more_functions.validate_input_functions import score_input
 
 
 class Test(unittest.TestCase):
+    def test_score_input_name(self):
+        try:
+            score_input()
+        except TypeError:
+            print("type error 1")
+
     def test_score_input_test_score_below_range(self):
-        self.assertLess(score_input("test", -3), 0)
+        try:
+            self.assertLess(score_input("test", -3), 0)
+        except AssertionError:
+            print("assert error 1")
 
     def test_score_input_test_score_above_range(self):
-        self.assertGreater(score_input("testname", 200), 100)
+        try:
+            self.assertGreater(score_input("testname", 200), 100)
+        except AssertionError:
+            print("assert error 2")
 
     def test_test_score_non_numeric(self):
         try:
             score_input("a", "x")
         except TypeError:
-            print("type error")
+            print("type error 2")
 
 
 if __name__ == "__main__":
