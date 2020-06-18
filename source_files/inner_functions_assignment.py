188de94f957f7fe34a35115f4bb6cfd456861103
chasevanblair <crvanblair@dmacc.edu>
2020-06-17 21:18:52 -0500

    inner functions 2

--- source_files/inner_functions_assignment.py
+++ source_files/inner_functions_assignment.py
@@ -1,9 +1,17 @@
 def measurements(the_list):
-    def area(a_list):
-        return a_list[0] * a_list[1]
+    if len(the_list) == 2:
+        def area(a_list):
+            return a_list[0] * a_list[1]
 
-    def perimeter(a_list):
-        return 2 * a_list[0] + 2 * a_list[1]
+        def perimeter(a_list):
+            return 2 * a_list[0] + 2 * a_list[1]
 
-    return "Perimeter = %s Area = %s" % (perimeter(the_list), area(the_list))
+        return "Perimeter = %s Area = %s" % (perimeter(the_list), area(the_list))
+    elif len(the_list) == 1:
+        def area(a_list):
+            return a_list[0] ** 2
 
+        def perimeter(a_list):
+            return a_list[0] * 4
+
+        return "Perimeter = %s Area = %s" % (perimeter(the_list), area(the_list))
