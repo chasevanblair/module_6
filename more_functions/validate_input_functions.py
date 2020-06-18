e5447c3e693a9de32d6457cd770e0ad850d9d0ca
chasevanblair <crvanblair@dmacc.edu>
2020-06-17 20:08:40 -0500

    final test

--- more_functions/validate_input_functions.py
+++ more_functions/validate_input_functions.py
@@ -1,4 +1,26 @@
+"""
+
+Program: validate_input_functions.py
+
+Author: Chase Van Blair
+
+Last date modified: 6/17/20
+
+
+The purpose of this program is to accept test name, test score, and a message to display if invalid
+then print invalid message if needed and return test name and test score
+
+"""
+
+
 def score_input(test_name, test_score=0, invalid_message="Invalid test score, try again!"):
+    """
+    takes parameters, checks them, raises error if wrong, then returns test name and test score
+    :param test_name:
+    :param test_score:
+    :param invalid_message:
+    :return test_name, test_score:
+    """
     if test_score < 0 or test_score > 100:
         print(invalid_message)
         raise ValueError
--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -1,3 +1,16 @@
+"""
+
+Program: test_valid_input_in_functions.py
+
+Author: Chase Van Blair
+
+Last date modified: 6/17/20
+
+
+The purpose of this program is to test validate_input_functions
+to make sure it works correctly
+
+"""
 import unittest
 
 from more_functions.validate_input_functions import score_input
@@ -5,34 +18,58 @@ from more_functions.validate_input_functions import score_input
 
 class Test(unittest.TestCase):
     def test_score_input_name(self):
+        """
+        test score input name is a string
+        """
         try:
             score_input("x")
         except TypeError:
             print("type error 1")
 
     def test_score_input_test_score_valid(self):
+        """
+        test score input is valid
+        """
         try:
             score_input("x", 60, "invalid")
         except ValueError:
             print("invalid test score")
 
     def test_score_input_test_score_below_range(self):
+        """
+        test if score input is below 0
+        """
         try:
             score_input("test", -3)
         except ValueError:
             print("Input less than 0")
 
     def test_score_input_test_score_above_range(self):
+        """
+        test if score input is above 100
+        """
         try:
             score_input("testname", 200)
         except ValueError:
             print("Input greater than 100")
 
     def test_test_score_non_numeric(self):
+        """
+        test if score input is numeric
+        """
         try:
             score_input("a", "x")
         except TypeError:
             print("test score is not numeric")
+
+    def test_score_input_invalid_message(self):
+        """
+        test the invalid message input
+        """
+        try:
+            score_input("s", 28, "invalid")
+        except TypeError:
+            print("type error with invalid message")
 
 
 if __name__ == "__main__":
