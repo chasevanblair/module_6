730e089f70bc487b2ce14da14f25bd38e48588d3
chasevanblair <crvanblair@dmacc.edu>
2020-06-17 19:58:11 -0500

    test for ranges

--- more_functions/validate_input_functions.py
+++ more_functions/validate_input_functions.py
@@ -1,9 +1,11 @@
-def score_input(test_name: str, test_score=0, invalid_message="Invalid test score, try again!"):
-    while 100 < test_score < 0:
+def score_input(test_name, test_score=0, invalid_message="Invalid test score, try again!"):
+    if test_score < 0 or test_score > 100:
+        print(invalid_message)
+        raise ValueError
+    if type(test_score) != int:
+        raise TypeError
         print(invalid_message)
     # return {test_name: test_score}
 
     return test_name, test_score
 
-
-#score_input()
--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -6,27 +6,33 @@ from more_functions.validate_input_functions import score_input
 class Test(unittest.TestCase):
     def test_score_input_name(self):
         try:
-            score_input()
+            score_input("x")
         except TypeError:
             print("type error 1")
 
+    def test_score_input_test_score_valid(self):
+        try:
+            score_input("x", 60, "invalid")
+        except ValueError:
+            print("invalid test score")
+
     def test_score_input_test_score_below_range(self):
         try:
-            self.assertLess(score_input("test", -3), 0)
-        except AssertionError:
-            print("assert error 1")
+            score_input("test", -3)
+        except ValueError:
+            print("Input less than 0")
 
     def test_score_input_test_score_above_range(self):
         try:
-            self.assertGreater(score_input("testname", 200), 100)
-        except AssertionError:
-            print("assert error 2")
+            score_input("testname", 200)
+        except ValueError:
+            print("Input greater than 100")
 
     def test_test_score_non_numeric(self):
         try:
             score_input("a", "x")
         except TypeError:
-            print("type error 2")
+            print("test score is not numeric")
 
 
 if __name__ == "__main__":
